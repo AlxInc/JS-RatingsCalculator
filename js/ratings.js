2f7b0e7ccb98e2bed3716802bd61c7e982369d90
AlxInc <alex.haynes@live.com.au>
2022-08-12 17:53:14 +0930

    initial commit'

---
+++ js/ratings.js
@@ -0,0 +1,28 @@
+function collect_ratings() {
+    const ratings = Object.create();
+    ratings.count = 0
+    ratings.sum = 0
+    ratings.average = 0 
+
+    let rating = 0
+    document.querySelectorAll('rating')
+    const elements[] = rating
+
+    elements.array.forEach(element => {
+        element.id.replace('star', "")
+        parseInt(element.value, count++);
+        parseInt(element.value * rating);
+    });
+
+    if (ratings.count != 0) {
+        ratings.average = sum / count;
+        
+    }
+
+    return ratings;
+}
+
+document.addEventListener('change' event =>
+const ratings = event
+id.querySelector(average = ratings.average, 2)
+)
