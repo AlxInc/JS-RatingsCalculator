ad1e6504bbc0096608be7099cdfa88f7f8fab122
AlxInc <alex.haynes@live.com.au>
2022-08-12 18:19:15 +0930

    trying again

--- js/ratings.js
+++ js/ratings.js
@@ -1,18 +1,21 @@
 function collect_ratings() {
     const ratings = Object.create();
-    ratings.count = 0
-    ratings.sum = 0
-    ratings.average = 0 
+    ratings.count = 0;
+    ratings.sum = 0;
+    ratings.average = 0;
 
     let rating = 0
-    document.querySelectorAll('rating')
-    const elements[] = rating
-
+    const elements[] = document.querySelectorAll('rating');
     elements.array.forEach(element => {
-        element.id.replace('star', "")
-        parseInt(element.value, count++);
-        parseInt(element.value * rating);
-    });
+        element.id = "star".replace("");
+        rating = parseInt("");
+        parseInt(element.value, ratings.count++);
+        Object.ratings.sum = parseInt(element.value * rating);;
+        });
+    
+    
+
+    
 
     if (ratings.count != 0) {
         ratings.average = sum / count;
